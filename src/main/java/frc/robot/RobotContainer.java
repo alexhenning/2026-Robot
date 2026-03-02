36c44ff4abcf91f5c1739cc4e4441a788a0f6ef1
Team-348-Programming <348progamming@gmail.com>
2026-03-02 15:11:21 -0500

    Fixed the accedential deletion of a bunch of stuff in robotcontainer and robot and undo change was not working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,11 @@ package frc.robot;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ClimbC;
-
+import frc.robot.commands.ClimbC;
+import frc.robot.commands.IntakeC;
+import frc.robot.commands.IntakeTiltC;
+import frc.robot.commands.LauncherC;
+import frc.robot.commands.LauncherSpeedC;
 import frc.robot.subsystems.*;
 
 import com.pathplanner.lib.commands.PathPlannerAuto;
@@ -24,10 +28,15 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private static final DriveSubsystem m_robotDrive = new DriveSubsystem();
-
+  public static final VisionSS rc_visionSS = new VisionSS();
+  public static final IntakeTiltSS rc_IntakeTiltSS = new IntakeTiltSS();
+  public static final IntakeSS rc_intakeSS = new IntakeSS();
+  public static final LauncherSS rc_launcherSS = new LauncherSS();
   public static final ClimbSS rc_climbSS = new ClimbSS();
 
-
+  public static final IntakeC rc_intakeC = new IntakeC(rc_intakeSS);
+  public static final LauncherC rc_launcherC = new LauncherC(rc_launcherSS);
+  public static final LauncherSpeedC rc_launcherspeedC = new LauncherSpeedC(rc_launcherSS, 0.5);
   public static final ClimbC rc_climbC = new ClimbC(rc_climbSS);
 
   public static final CommandXboxController m_driverController =
@@ -35,7 +44,14 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
+     rc_visionSS.setDefaultCommand(
+      new RunCommand(
+          () ->
+            rc_visionSS.PrintTarget(),
+            rc_visionSS
+            
+      )
+    );
     // Configure the trigger bindings
     configureBindings();
     // Configure default commands
@@ -73,6 +89,14 @@ public class RobotContainer {
     m_driverController.povUp().whileTrue(rc_climbC);
     m_driverController.povDown().whileTrue(rc_climbC);
 
+    m_driverController.povRight().onTrue(new IntakeTiltC(rc_IntakeTiltSS, () -> 10));
+    m_driverController.povLeft().onTrue(new IntakeTiltC(rc_IntakeTiltSS, () -> 0));
+
+    m_driverController.povRight().whileTrue(rc_intakeC);
+    m_driverController.povLeft().whileTrue(rc_intakeC);
+
+    m_driverController.a().whileTrue(rc_intakeC);
+    m_driverController.b().whileTrue(rc_intakeC);
     //Climb PID
 
     // m_driverController.povUp().onTrue(new LauncherSpeedC(rc_launcherSS, 1));
